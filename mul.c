14862c72dbda7e8a8285fbfa5de9243a66e73e2d
pablo.joaquim <pablo.joaquim@gmail.com>
2025-12-29 18:05:48 -0300

    Some comments added to the functions in the multiply module

--- mul.c
+++ mul.c
@@ -1,8 +1,8 @@
 /***************************************** 
  * New header for this function
- * Description
- * Inputs
- * Outputs
+ * Description Function to multiply two numbers
+ * Inputs a and b are the two numbers to multiply
+ * Outputs The result of the operation
  *****************************************/
 int mul(int a, int b)
 {
